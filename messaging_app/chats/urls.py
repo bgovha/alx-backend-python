ddd35fb415a829cd161227a677d950baf7b181d9
Brenda <brendagovha1@gmail.com>
2025-11-28 21:17:09 +0200

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ConversationViewSet, MessageViewSet
 
-#routers.DefaultRouter
+#routers.DefaultRouter()
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'conversations', ConversationViewSet)
